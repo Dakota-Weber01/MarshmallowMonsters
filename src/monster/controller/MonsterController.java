e7cb205a9f5e65cb3b9ac8655e315a30ad7bec54
Dakota Weber <dakota.weber01@gmail.com>
2017-09-15 13:32:37 -0600

    Added interactions with my monster, added a scanner in my controller. Add two more strings in my model package.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,11 +1,36 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+import java.util.Scanner;
+
 public class MonsterController
 {
 
 	public void start()
 	{
+		MarshmallowMonster basic = new MarshmallowMonster();
+		System.out.println(basic);
+		MarshmallowMonster yeet = new MarshmallowMonster("Stinky Yeet Monster", 2, 6, 1, true);
+		System.out.println(yeet);
+		System.out.println("I am feeling hungry, I'm going to eat one of Yeet's arms");
+		yeet.setArmCount(yeet.getArmCount()-1);
+		{
+		interactWithMonster(yeet); }
+		}
+	
 		
+			private void interactWithMonster(MarshmallowMonster currentMonster)
+			{
+		{
+			Scanner myScanner = new Scanner(System.in);
+			System.out.println(currentMonster.getName() + "wants to know how many eyes you want to eat, please type how many");
+			currentMonster.setEyeCount(currentMonster.getEyeCount() - Consumed);
+			int Consumed = myScanner.nextInt();
+			System.out.println(currentMonster);
+		
+	myScanner.close();
+		}
 	}
-
+	
 }
+ 
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -17,7 +17,7 @@ public class MarshmallowMonster
 	}
 
 	
-	public MarshmallowMonster(String name, int eyeCount, int armCount, int tentacleCount, boolean hasBloop) 
+	public MarshmallowMonster(String name, int eyeCount, int armCount, double tentacleCount, boolean hasBloop) 
 	{
 		this.name = name;
 		this.eyeCount = eyeCount;
@@ -25,15 +25,54 @@ public class MarshmallowMonster
 		this.tentacleAmount = tentacleCount;
 		this.hasBloop = hasBloop;
 	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	public void setname(String name)
+	{
+		this.name = name;
+	}
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	public void setTentacleAmount(int tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	public void setHasBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
 	
 	public String toString()
 	{
-	String description = "I am a stinky monster, my name is" + name + "I have" + eyeCount +  "eyes and only";
-	description += tentacleAmount + "tentacles but I have" + armCount + "arms!." + hasBloop + "tells you";
-	description += "if I have a bloop"; 
+	String description = "I am a stinky monster, my name is " + name + " I have" + eyeCount +  " eyes and only";
+	description += tentacleAmount + " tentacles but I have" + armCount + " arms!." + hasBloop + "tells you";
+	description += " if I have a bloop"; 
 	return description;
 	}
-	
 	
 }
 
