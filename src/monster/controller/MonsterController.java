0acfe8e6ac37b6535a054ef28c448a64283d1aa3
Dakota Weber <dakota.weber01@gmail.com>
2017-09-26 12:35:26 -0600

    Fixing Bugs.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -12,31 +12,50 @@ public class MonsterController
 	{
 		popup = new MonsterDisplay();
 	}
+	
+
 	public void start()
+	
 	{
+//	{
+//		int count = 0; 
+//		while(count < 10)
+//		{
+//			popup.displayText("Am I not the coolest?!");
+//			count +=2;
+//		}
+	
+		for(int loop = 0; loop < 10; loop += 1)
+		{
+			popup.displayText("This is loop # " + (loop + 1) + "of ten");
+		}
+	}
 		MarshmallowMonster basic = new MarshmallowMonster();
-//		System.out.println(basic);
+		//		System.out.println(basic);
 		popup.displayText(basic.toString());
 		MarshmallowMonster yeet = new MarshmallowMonster("Stinky Yeet Monster", 2, 6, 1, true);
 //		System.out.println(yeet);
+		popup.out.println(yeet);
 		popup.displayText(yeet.toString());
 //		System.out.println("I am feeling hungry, I'm going to eat one of Yeet's arms");
+		popup.displayText("I am feeling hungry, I'm going to eat one of Yeet's arms");
 		popup.displayText(yeet.toString());
 		yeet.setArmCount(yeet.getArmCount()-1);
 		{
-		interactWithMonster(yeet); }
+		interactWithMonster(yeet); 
 		}
+		
 			private void interactWithMonster(MarshmallowMonster currentMonster)
 			// Classes and imported objects scanners, displays etc.
 			{
 		{
 			Scanner myScanner = new Scanner(System.in);
 //			System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, please type how many");
-//			int consumed;
+			int consumed;
 //			String response = int consumed = popup.getResponse(currentMonster.getName() + " wants to know how many eyes you want to eat, please type how many");
-//			if (isValidInteger(response))
-//			consumed = Integer.parseInt(response);
-////			int Consumed = myScanner.nextInt();
+			if (isValidInteger(response))
+			consumed = Integer.parseInt(response);
+//			int Consumed = myScanner.nextInt();
 			currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 			System.out.println(currentMonster);
 			System.out.println("How many arms are you interested in eating?, I have " + currentMonster.getArmCount()); 
@@ -71,30 +90,29 @@ public class MonsterController
 			}
 			popup.displayText("Hi there, ready to play?");
 			String answer = popup.getResponse("What is the air speed of a coconut laden swallow?");
-			System.out.println(answer);
+//			System.out.println(answer);
 			//helper methods
-//			private boolean isValidInteger(String sample)
-//			{
-//				boolean vaild = false;
-//				try
-//			}
-//			{
-//				Integer.parseInt(sample);
-//				valid = true;
-//			}
-//			{
-//				catch(NumberFormatException error)
-//			}
-//			{
-//				popup.displayText("You need to type in a double -" + sampleDouble)
-//			}
-//			{
-//				popup.displayText("You need to input an int, " + sample + "is not valid.");
-//			}
-//			myScanner.close();		
-//		
-//		
-//		}		
+			private boolean isValidInteger(String sample)
+			{
+				variable vaild = false;
+			}
+			{
+				Integer.parseInt(sample);
+				valid = true;
+			}
+			{
+				catch(NumberFormatException error)
+			}
+			{
+				popup.displayText("You need to type in a double -" + sampleDouble)
+			}
+			{
+				popup.displayText("You need to input an int, " + sample + "is not valid.");
+			}
+			myScanner.close();		
+		
+		
+	}		
 			
 			
 			
